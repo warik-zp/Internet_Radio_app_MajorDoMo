8422769a8d237d9dc9574546af025c3a429068c6
Egor <savenko.zp@gmail.com>
2017-12-02 20:39:41 +0200

    Big update Added view fucntion Added db install function Added admin view Added action in admin view Added template files Added .gitignore

--- modules/internetradio/internetradio.class.php
+++ modules/internetradio/internetradio.class.php
@@ -119,6 +119,34 @@ function run() {
 * @access public
 */
 function admin(&$out) {
+  if (isset($this->data_source) && !$_GET['data_source'] && !$_POST['data_source']) {
+    $out['SET_DATASOURCE'] = 1;
+  }
+  if ($this->data_source == 'app_radio' || $this->data_source == '') {
+    $out['VER'] = '1.3.1';
+    global $select_terminal;
+    if ($select_terminal != '')
+      setGlobal('RadioSetting.PlayTerminal', $select_terminal);
+    $out['PLAY_TERMINAL'] = getGlobal('RadioSetting.PlayTerminal');
+    $res = SQLSelect("SELECT NAME FROM terminals");
+    if ($res[0]) {
+      $out['LIST_TERMINAL'] = $res;
+    }
+
+    if ($this->view_mode == '' || $this->view_mode == 'view_stations') {
+      $this->view_stations($out);
+    }
+    if ($this->view_mode == 'edit_stations') {
+      $this->edit_stations($out, $this->id);
+    }
+    if ($this->view_mode == 'delete_stations') {
+      $this->delete_stations($this->id);
+      $this->redirect("?");
+    }
+    if ($this->view_mode == 'import_stations') {
+      $this->import_stations($out);
+    }
+  }
 }
 
 /**
@@ -132,6 +160,49 @@ function usual(&$out) {
  $this->admin($out);
 }
 
+function view_stations(&$out) {
+  //require(DIR_MODULES.$this->name.'/view_stations.php');
+  $table_name = 'internetradio';
+  $res = SQLSelect("SELECT * FROM $table_name");
+  if ($res[0][ID]) {
+    $out['RESULT'] = $res;
+  }
+}
+
+function edit_stations(&$out, $id) {
+  //require(DIR_MODULES.$this->name.'/view_stations.php');
+  $table_name = 'internetradio';
+  $rec = SQLSelectOne("SELECT * FROM $table_name WHERE ID='$id'");
+
+  if ($this->mode == 'update') {
+    $ok = 1;
+    //updating 'stations' (text, required)
+    global $stations;
+    global $name;
+    $rec['URL'] = $stations;
+    $rec['Name'] = $name;
+    if ($rec['URL'] == '' || $rec['Name'] == '') {
+      $out['ERR_stations'] = 1;
+      $ok = 0;
+    }
+    //UPDATING RECORD
+    if ($ok) {
+      if ($rec['ID']) {
+        SQLUpdate($table_name, $rec); // update
+      } else {
+        $new_rec = 1;
+        $rec['ID'] = SQLInsert($table_name, $rec); // adding new record
+      }
+      $out['OK'] = 1;
+    } else {
+      $out['ERR'] = 1;
+    }
+  }
+  outHash($rec, $out);
+}
+
+
+
 /**
 * Install
 *
@@ -142,6 +213,18 @@ function usual(&$out) {
  function install() {
   parent::install();
  }
+
+
+ function dbInstall($data)
+ {
+$data = <<<EOD
+internetradio: ID int(10) unsigned NOT NULL auto_increment
+internetradio: Name text
+internetradio: URL text
+EOD;
+     parent::dbInstall($data);
+ }
+
 // --------------------------------------------------------------------
 }
 ?>
